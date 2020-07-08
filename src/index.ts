5c3ac3a863f01da02e3b38b3444b832b1a36b569
Matteo Cosenza <m.cosenza@smarterlab.it>
2020-07-08 12:48:09 +0200

    Primry Button Added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import * as React from 'react'
+import { PrimaryButton } from '@fluentui/react'
+
 export const Greeter = (name: string) => `Hello ${name}`
+export const SLButton = () => {
+  return PrimaryButton
+}
